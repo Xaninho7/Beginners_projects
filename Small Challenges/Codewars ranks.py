905c48e8ea409e850996498ffeb1c80c2d3a504b
Cristiano <Xaninhonl@gmai.com>
2022-01-30 21:44:02 +0100

    Final

---
+++ Small Challenges/Codewars ranks.py
@@ -0,0 +1,11 @@
+class User:
+    ranks = [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8
+             ]
+    def __init__(self):
+        self.rank = 0
+        self.progress = 0
+
+
+    def inc_progress(self):
+        pass
+
